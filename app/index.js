5029f03d7cc641bd7b62eb3ff5df241758820467
shngmsw <mose1021@gmail.com>
2021-01-25 23:31:32 +0900

    fix import TTS

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const Handler = require("./handler.js");
 const Dispandar = require("./dispandar.js");
+const TTS = require("./tts/voice_bot_node.js");
 client.login(process.env.DISCORD_BOT_TOKEN);
 
 client.on("message", async (msg) => {
