3ae8b1c50b38210650702dd78269e5b0ca038941
ShingoMisawa <33051481+shngmsw@users.noreply.github.com>
2021-03-08 17:59:36 +0900

    add replacestar

--- app/common.js
+++ app/common.js
@@ -2,7 +2,8 @@ const getPrefixes = require('../db/prefixes_select.js');
 const DEFAULT_PREFIX = '!!';
 module.exports = {
     isInteger: isInteger,
-    getPrefix: getPrefix
+    getPrefix: getPrefix,
+    replaceStar: replaceStar
 }
 
 function isInteger(x) {
@@ -13,7 +14,15 @@ async function getPrefix(serverId) {
     let prefix = DEFAULT_PREFIX;
     const serverPrefix = await getPrefixes(serverId);
     if (serverPrefix[0] != null) {
-        prefix = serverPrefix.prefix;
+        prefix = serverPrefix[0].prefix;
     }
     return prefix;
 }
+
+function replaceStar(num) {
+    console.log(num)
+    if (num == 0 || !num ) {
+      return "❌";
+    }
+    return Array(1 + Number(num)).join('⭐');
+  }
