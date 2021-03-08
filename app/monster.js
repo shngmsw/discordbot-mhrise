57ef6d3da62abffb1c67cb36659c355776c6648a
ShingoMisawa <33051481+shngmsw@users.noreply.github.com>
2021-03-08 18:00:15 +0900

    fix monster and await getPrefix

--- app/monster.js
+++ app/monster.js
@@ -2,7 +2,7 @@ const common = require("./common.js");
 const getMonster = require("../db/monsters_select.js");
 
 module.exports = async function handleMonster(msg) {
-    const prefix = common.getPrefix(msg.guild.id);
+    const prefix = await common.getPrefix(msg.guild.id);
     var strCmd = msg.content.replace(prefix, "");
     var name = strCmd;
 
@@ -15,19 +15,19 @@ module.exports = async function handleMonster(msg) {
                 },
                 color: 0xf02d7d,
                 fields: [
-                    { name: '火', value: replaceStar(monsters[i].weak_fire), inline: true },
-                    { name: '水', value: replaceStar(monsters[i].weak_water), inline: true },
-                    { name: '雷', value: replaceStar(monsters[i].weak_thunder), inline: true },
-                    { name: '氷', value: replaceStar(monsters[i].weak_ice), inline: true },
-                    { name: '龍', value: replaceStar(monsters[i].weak_dragon), inline: true },
-                    { name: '毒', value: replaceStar(monsters[i].weak_poison), inline: true },
-                    { name: '眠り', value: replaceStar(monsters[i].weak_sleep), inline: true },
-                    { name: '麻痺', value: replaceStar(monsters[i].weak_paralysis), inline: true },
-                    { name: '爆破', value: replaceStar(monsters[i].weak_blast), inline: true },
-                    { name: '気絶', value: replaceStar(monsters[i].weak_stun), inline: true }
+                    { name: '火', value: common.replaceStar(monsters[i].weak_fire), inline: true },
+                    { name: '水', value: common.replaceStar(monsters[i].weak_water), inline: true },
+                    { name: '雷', value: common.replaceStar(monsters[i].weak_thunder), inline: true },
+                    { name: '氷', value: common.replaceStar(monsters[i].weak_ice), inline: true },
+                    { name: '龍', value: common.replaceStar(monsters[i].weak_dragon), inline: true },
+                    { name: '毒', value: common.replaceStar(monsters[i].weak_poison), inline: true },
+                    { name: '眠り', value: common.replaceStar(monsters[i].weak_sleep), inline: true },
+                    { name: '麻痺', value: common.replaceStar(monsters[i].weak_paralysis), inline: true },
+                    { name: '爆破', value: common.replaceStar(monsters[i].weak_blast), inline: true },
+                    { name: '気絶', value: common.replaceStar(monsters[i].weak_stun), inline: true }
                 ],
                 "image": {
-                    "url": getImgUrl(monsters[i].icon_url)
+                    "url": monsters[i].icon_url
                 }
             }
         }
