32195a29adc861a7b79067df00e0ccfbb100218c
shngmsw <mose1021@gmail.com>
2021-03-26 20:09:29 +0900

    fix update_at spel

--- db/prefixes_insert.js
+++ db/prefixes_insert.js
@@ -5,7 +5,7 @@ module.exports = async function insert(id, prefix) {
     try {
         const sql = "INSERT INTO prefixes (server_id, prefix, created_at, updated_at) VALUES ($1, $2, NOW(), NOW()) "
             + "ON CONFLICT ON CONSTRAINT prefixes_pkey "
-            + "DO UPDATE SET prefix=$2, updatead_at=NOW()";
+            + "DO UPDATE SET prefix=$2, updated_at=NOW()";
         const params = [id, prefix];
 
         await db.begin();
