f86baaabf516bb5b957ac7eb6d427ddea7605fcf
shngmsw <mose1021@gmail.com>
2021-03-26 20:07:40 +0900

    fix upsert timestamp

--- db/prefixes_insert.js
+++ db/prefixes_insert.js
@@ -3,9 +3,9 @@ var { getPostgresClient } = require('./db.js');
 module.exports = async function insert(id, prefix) {
     const db = await getPostgresClient();
     try {
-        const sql = "INSERT INTO prefixes (server_id, prefix, created_at, updated_at) VALUES ($1, $2, UTC_TIMESTAMP(), UTC_TIMESTAMP()) "
+        const sql = "INSERT INTO prefixes (server_id, prefix, created_at, updated_at) VALUES ($1, $2, NOW(), NOW()) "
             + "ON CONFLICT ON CONSTRAINT prefixes_pkey "
-            + "DO UPDATE SET prefix=$2, updatead_at=UTC_TIMESTAMP()";
+            + "DO UPDATE SET prefix=$2, updatead_at=NOW()";
         const params = [id, prefix];
 
         await db.begin();
