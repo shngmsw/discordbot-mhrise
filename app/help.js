6e9b789fe552d3bfe21f0a20a99f508c46031a90
shngmsw <mose1021@gmail.com>
2021-03-27 14:12:24 +0900

    add WebSiteLink to HelpEmebed

--- app/help.js
+++ app/help.js
@@ -1,4 +1,5 @@
 const common = require("./common.js");
+const URL = process.env.NETLIFY_URL;
 module.exports = async function handleHelp(msg) {
     const prefix = await common.getPrefix(msg.guild.id);
     var strCmd = msg.content.replace(/　/g, ' ');
@@ -12,6 +13,7 @@ module.exports = async function handleHelp(msg) {
             author: {
                 name: 'MHRiseBOTの使い方'
             },
+            description: `[モンスター一覧、編集や追加はこちら](${URL})`,
             color: 0x1bc2a5,
             fields: [{
                 name: 'モンスターの弱点属性表示\r\n例：' + prefix + 'ディアブロス or 角竜',
